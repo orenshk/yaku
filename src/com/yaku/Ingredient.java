709a3c0cac613c6847d834ef69d6d8f529c4d0c2
Oren Shklarsky <oren@neur.io>
2015-01-17 10:59:35 -0800

    initial commit

---
+++ src/com/yaku/Ingredient.java
@@ -0,0 +1,8 @@
+package com.yaku;
+
+/**
+ * Created by orenshklarsky on 14-12-13.
+ * Interface defining an Ingredient object.
+ */
+public interface Ingredient {
+}
---
+++ src/com/yaku/Main.java
@@ -0,0 +1,8 @@
+package com.yaku;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
